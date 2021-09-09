3ef3c31a72eb502c499b0bf53e7cdbf396dc7869
Spencer Gritton <spencergritton@Spencers-MacBook-Pro.local>
2021-09-08 22:27:51 -0400

    Fixed youtube playback issues, added readme, restructured project

---
+++ Chrome/background.js
@@ -0,0 +1,73 @@
+// Initialize plugin on install
+chrome.runtime.onInstalled.addListener(function () {
+	console.log('Installed plugin');
+	chrome.storage.sync.set({
+		increaseSpeedKey: 'D',
+		decreaseSpeedKey: 'S',
+		toggleOverlayKey: 'Q',
+		speedModifier: 0.1,
+		persistentCurrentSpeed: 1.0,
+		persistentSpeed: true,
+		showOverlay: true,
+		logLevel: 0,
+	});
+});
+
+// Listens for updates on SPA's and executes content script when update occurs
+chrome.webNavigation.onHistoryStateUpdated.addListener(
+	debounce(function () {
+		chrome.tabs.executeScript(null, { file: 'content.js' });
+	}, 250)
+);
+
+// Listen for messages from content script
+chrome.runtime.onMessage.addListener(function (request) {
+	// Logger
+	if (request.type === 'console') {
+		console.log(request.message);
+	}
+	// Listen for request to refresh current page
+	else if (request.type === 'refresh') {
+		chrome.tabs.query(
+			{ active: true, currentWindow: true },
+			function (arrayOfTabs) {
+				var code = 'window.location.reload();';
+				chrome.tabs.executeScript(arrayOfTabs[0].id, { code: code });
+			}
+		);
+	}
+});
+
+// Debounce helper
+function debounce(func, wait, immediate) {
+	var timeout;
+
+	return function executedFunction() {
+		var context = this;
+		var args = arguments;
+
+		var later = function () {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+
+		var callNow = immediate && !timeout;
+
+		clearTimeout(timeout);
+
+		timeout = setTimeout(later, wait);
+
+		if (callNow) func.apply(context, args);
+	};
+}
+
+// Listens for URL changes on SPA's (before they change DOM)
+// If url change send message to content script telling it to clear event listeners
+// before page update
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	if (changeInfo.url) {
+		chrome.tabs.sendMessage(tabId, {
+			message: 'url_change',
+		});
+	}
+});
---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple Video Controller
+
+The desire for this project was to create a simple and easily auditable HTML video speed controller.
+No longer will you have to worry that the 5,000 line video controller you downloaded is doing something sneaky behind your back.
+
+# Current Functionality
+
+-   Set hotkeys to control video speed
+-   Show or hide current video speed (also hotkey-able)
+-   Set a persistent speed which will carry through videos (possibly a WIP)
+-   Change speed modifier (default 0.1x)
+-   Plugin is 486 lines of code, meaning most novice developers should be able to audit it without much trouble
+
+# Future Goals
+
+-   Add Firefox Compatibility
+-   Publish on Chrome Web Store
+-   Add hotkeys for speed pre-sets
+-   Create logo
+-   Improve popup menu styling
+-   Improve plugin wording
